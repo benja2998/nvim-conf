99d3f9d499856725d9ba3cb458875b815361197c
benja2998 <benja2998@duck.com>
2025-10-15 21:10:56 -0300

    Add <leader>e to open file explorer

--- init.lua
+++ init.lua
@@ -5,6 +5,14 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.wrap = false
+vim.opt.linebreak = true
+vim.opt.showmode = false
+
 -- bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -16,10 +24,312 @@ vim.opt.rtp:prepend(lazypath)
 vim.opt.cursorline = true
 
 require("lazy").setup({
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-    {"neovim/nvim-lspconfig"},
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha",
+                transparent_background = false,
+                integrations = {
+                    treesitter = true,
+                    telescope = true,
+                    native_lsp = {
+                        enabled = true,
+                    },
+                },
+            })
+            vim.cmd.colorscheme "catppuccin"
+        end,
+    },
+
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require('lualine').setup({
+                options = {
+                    theme = 'catppuccin',
+                    component_separators = { left = '|', right = '|'},
+                    section_separators = { left = '', right = ''},
+                    globalstatus = true,
+                },
+                sections = {
+                    lualine_a = {'mode'},
+                    lualine_b = {'branch', 'diff', 'diagnostics'},
+                    lualine_c = {'filename'},
+                    lualine_x = {'encoding', 'fileformat', 'filetype'},
+                    lualine_y = {'progress'},
+                    lualine_z = {'location'}
+                },
+            })
+        end,
+    },
+
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        config = function()
+            require("ibl").setup({
+                indent = {
+                    char = "│",
+                },
+                scope = {
+                    enabled = true,
+                    show_start = false,
+                    show_end = false,
+                },
+            })
+        end,
+    },
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "lua", "vim", "vimdoc" },
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+                indent = { enable = true },
+            })
+        end,
+    },
+
+    -- Mason: LSP server manager
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗"
+                    }
+                }
+            })
+        end,
+    },
+
+    -- Bridge between mason and lspconfig
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "williamboman/mason.nvim" },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "pyright",
+                    "ts_ls",
+                    "rust_analyzer",
+                },
+                automatic_installation = true,
+            })
+        end,
+    },
+
+    -- Autocompletion plugins
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+        },
+        config = function()
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                formatting = {
+                    format = function(entry, vim_item)
+                        vim_item.menu = ({
+                            nvim_lsp = "[LSP]",
+                            luasnip = "[Snippet]",
+                            buffer = "[Buffer]",
+                            path = "[Path]",
+                        })[entry.source.name]
+                        return vim_item
+                    end,
+                },
+            })
+
+            -- Command line completion
+            cmp.setup.cmdline(":", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = "path" }
+                }, {
+                    { name = "cmdline" }
+                }),
+            })
+
+            -- Search completion
+            cmp.setup.cmdline("/", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = "buffer" }
+                },
+            })
+        end,
+    },
+
+    -- LSP Configuration
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+        },
+        config = function()
+            -- LSP keybindings
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+                callback = function(ev)
+                    local opts = { buffer = ev.buf, silent = true }
+                    
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+                    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+                    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+                    vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+                end,
+            })
+
+            -- Enhanced capabilities for autocompletion
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+            -- Setup LSP servers using vim.lsp.config
+            local servers = {
+                lua_ls = {
+                    settings = {
+                        Lua = {
+                            diagnostics = {
+                                globals = { "vim" },
+                            },
+                            workspace = {
+                                library = vim.api.nvim_get_runtime_file("", true),
+                                checkThirdParty = false,
+                            },
+                        },
+                    },
+                },
+                pyright = {},
+                ts_ls = {},
+                rust_analyzer = {},
+            }
+            
+            for server, config in pairs(servers) do
+                config.capabilities = capabilities
+                vim.lsp.config[server] = config
+                vim.lsp.enable(server)
+            end
+
+            -- Diagnostic configuration with signs
+            vim.diagnostic.config({
+                virtual_text = true,
+                signs = {
+                    text = {
+                        [vim.diagnostic.severity.ERROR] = " ",
+                        [vim.diagnostic.severity.WARN] = " ",
+                        [vim.diagnostic.severity.HINT] = " ",
+                        [vim.diagnostic.severity.INFO] = " ",
+                    },
+                },
+                update_in_insert = false,
+                underline = true,
+                severity_sort = true,
+                float = {
+                    border = "rounded",
+                    source = "always",
+                },
+            })
+        end,
+    },
+
     { "nvim-tree/nvim-web-devicons" },
     { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+
+    {
+        "karb94/neoscroll.nvim",
+        config = function()
+            require('neoscroll').setup({
+                easing_function = "quadratic",
+            })
+        end,
+    },
+
+    {
+        "rcarriga/nvim-notify",
+        config = function()
+            vim.notify = require("notify")
+            require("notify").setup({
+                background_colour = "#000000",
+                render = "compact",
+                timeout = 2000,
+            })
+        end,
+    },
+
+    {
+        "NvChad/nvim-colorizer.lua",
+        config = function()
+            require('colorizer').setup()
+        end,
+    },
 
     {
         "MagicDuck/grug-far.nvim",
@@ -43,50 +353,32 @@ require("lazy").setup({
             },
         },
     },
-{
-  "christoomey/vim-tmux-navigator",
-  cmd = {
-    "TmuxNavigateLeft",
-    "TmuxNavigateDown",
-    "TmuxNavigateUp",
-    "TmuxNavigateRight",
-    "TmuxNavigatePrevious",
-    "TmuxNavigatorProcessList",
-  },
-  keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-  },
-}
+
+    {
+        "christoomey/vim-tmux-navigator",
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+            "TmuxNavigatorProcessList",
+        },
+        keys = {
+            { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+        },
+    },
 })
 
 vim.o.guifont = "CaskaydiaCove Nerd Font Mono:h14"
 vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })
 
--- Go to definition
-vim.keymap.set("n", "gd", "<Plug>(coc-definition)", { silent = true })
--- Show references
-vim.keymap.set("n", "gr", "<Plug>(coc-references)", { silent = true })
--- Hover info
-vim.keymap.set("n", "K", ":call CocActionAsync('doHover')<CR>", { silent = true })
 -- File explorer
 vim.keymap.set("n", "<leader>e", ":Ex<CR>", { silent = true })
--- Tab completion in insert mode
-vim.keymap.set("i", "<Tab>", 
-  [[coc#pum#visible() ? coc#pum#confirm() : "\<Tab>"]],
-  { expr = true, silent = true }
-)
-
--- Navigate windows with Ctrl + hjkl
---[[
-vim.keymap.set("n", "<C-h>", "<C-w>h", { silent = true })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { silent = true })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { silent = true })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { silent = true })
-]]
 
 -- Move to next buffer/tab
 vim.keymap.set("n", "<Tab>", ":tabnext<CR>", { silent = true })
@@ -99,23 +391,14 @@ vim.keymap.set("n", "<leader>c", ":bdelete<CR>", { silent = true })
 
 vim.api.nvim_set_keymap('n', '<leader>n', ':tabnew<CR>', { noremap = true, silent = true })
 
--- nvim-autopairs setup
---[[
-require('nvim-autopairs').setup({
-    check_ts = true,
-    enable_check_bracket_line = true,
-    enable_moveright = true,
-})
-]]
-
 -- Telescope setup
 local telescope = require("telescope")
 local actions = require("telescope.actions")
 
 telescope.setup({
     defaults = {
-        prompt_prefix = " ",
-        selection_caret = " ",
+        prompt_prefix = " ",
+        selection_caret = " ",
         path_display = { "smart" },
         sorting_strategy = "ascending",
         layout_strategy = "flex",
